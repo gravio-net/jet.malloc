790d9fa5d0cb680efc061f9424d17fd1f155926d
demuskov <demuskov@gmail.com>
2018-04-26 13:39:57 +0300

    Initial public release

---
+++ lib/_jm_tests_jet_malloc.cmd
@@ -0,0 +1,22 @@
+jet.malloc.exe test2 4 1 8 0 0 jet_malloc >> _jet.malloc.results
+jet.malloc.exe test2 4 2 16 0 0 jet_malloc >> _jet.malloc.results
+jet.malloc.exe test2 4 3 32 0 0 jet_malloc >> _jet.malloc.results
+jet.malloc.exe test2 4 4 64 0 0 jet_malloc >> _jet.malloc.results
+jet.malloc.exe test2 4 5 128 0 0 jet_malloc >> _jet.malloc.results
+jet.malloc.exe test2 4 6 256 0 0 jet_malloc >> _jet.malloc.results
+jet.malloc.exe test2 4 7 512 0 0 jet_malloc >> _jet.malloc.results
+jet.malloc.exe test2 4 8 1024 0 0 jet_malloc >> _jet.malloc.results
+jet.malloc.exe test2 4 9 2048 0 0 jet_malloc >> _jet.malloc.results
+jet.malloc.exe test2 4 10 4096 0 0 jet_malloc >> _jet.malloc.results
+jet.malloc.exe test2 4 11 8192 0 0 jet_malloc >> _jet.malloc.results
+jet.malloc.exe test2 4 12 16384 0 0 jet_malloc >> _jet.malloc.results
+jet.malloc.exe test2 4 13 32768 0 0 jet_malloc >> _jet.malloc.results
+jet.malloc.exe test2 4 14 65536 0 0 jet_malloc >> _jet.malloc.results
+jet.malloc.exe test2 4 15 131072 0 0 jet_malloc >> _jet.malloc.results
+jet.malloc.exe test2 4 16 262144 0 0 jet_malloc >> _jet.malloc.results
+jet.malloc.exe test2 4 17 524288 0 0 jet_malloc >> _jet.malloc.results
+jet.malloc.exe test2 4 18 1048576 0 0 jet_malloc >> _jet.malloc.results
+jet.malloc.exe test2 4 19 2097152 0 0 jet_malloc >> _jet.malloc.results
+
+
+
